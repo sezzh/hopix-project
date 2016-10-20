85ed5384a1e82b4b42ab7c87f7f4d60c9b0758a4
Ricardo-Pascual <textex204@gmail.com>
2016-10-20 15:27:21 -0500

    was added SQL-Alchemy

--- auth-service/sql/test.sql
+++ auth-service/sql/test.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE login;
+CREATE DATABASE lerya;
 
-\c login;
+\c lerya;
 
 CREATE TABLE person(
   id SERIAL NOT NULL PRIMARY KEY,
-  name VARCHAR(100)
+  name VARCHAR(100),
 );
 
 INSERT INTO person(name)
