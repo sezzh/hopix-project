5b88f5bd3ea4317c346f19a1edc8567774cd7568
sezzh <sezzhltd@gmail.com>
2016-10-27 15:37:55 -0500

    done auth configuration with localStrategy

--- web-platform/index.js
+++ web-platform/index.js
@@ -1,51 +1,56 @@
-const express = require('express')
 const http = require('http')
+const path = require('path')
+const express = require('express')
+const expressSession = require('express-session')
 const cookieParser = require('cookie-parser')
-const csrf = require('csurf')
 const bodyParser = require('body-parser')
-const routerAdmin = require('./apps/admin/controllers')
-const port = 3000
+const csrf = require('csurf')
+const flash = require('req-flash')
 const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
+const localStrategy = require('authentication/strategies').localStrategy
+const routerAdmin = require('apps/admin/controllers')
+const serializers = require('authentication/serializers')
+const port = process.env.WEBPLATFORM_PORT || 3000
 
-var user = { username: 'sezzh', password: 'tifis', id: 4 }
+// Web server initialize.
+const app = express()
+const server = http.createServer(app)
 
-var app = express()
-
-app.get('/', (req, res) => {
-  res.send('holi')
-})
+// Express Sessions configuration.
+var esOpts = {
+  secret: process.env.WEBPLATFORM_SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}
 
 // Static folders.
-app.use('/static', express.static(`${__dirname}/static`))
-app.use('/public', express.static(`${__dirname}/public`))
+app.use('/static', express.static(path.join(__dirname, 'static')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 // View engine.
 app.set('view engine', 'pug')
 app.set('views', './views')
 
-// crsf protection.
-app.use(cookieParser())
+// Parsers.
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// crsf protection.
 app.use(csrf({ cookie: true }))
+
+// Passport config
+app.use(expressSession(esOpts))
+app.use(flash())
 app.use(passport.initialize())
-
-// Passport test
-passport.use(new LocalStrategy((username, password, done) => {
-  if (!username === 'sezzh') {
-    return done(null, false, { message: 'incorrect username.' })
-  }
-  if (!password === 'tifis') {
-    return done(null, false, { message: 'Incorrect password' })
-  }
-
-  return done(null, user)
-}))
+app.use(passport.session())
+passport.use(localStrategy)
+passport.serializeUser(serializers.serializeUser)
+passport.deserializeUser(serializers.deserializeUser)
 
 // Internal Routers.
 app.use('/admin', routerAdmin)
 
-const server = http.createServer(app)
 server.listen(port, () => {
   console.log(`Express working on ${port}`)
 })
